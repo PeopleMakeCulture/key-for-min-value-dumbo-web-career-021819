a32e6ed99b42ac384f9f155be466d19a46ae4f63
Jing Cao <jingcao.yale@gmail.com>
2019-02-04 20:33:01 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,13 +2,5 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-  current_value = 10
-  name_hash.each do |key,value|
-    if value < current_value
-      current_value = value
-    end
-  end
-  if name_hash[key] == current_value
-    return name_hash[key]
-  end 
+  values = name_hash.collect {|key, value| value}.sort
 end
